7bea922d29c2ad0a4109d8d35e4cb598583d26d2
TuRChess <tiagorodchs@gmail.com>
2026-03-24 22:27:49 -0300

    Movimento do Player e Fisica de Pulo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,36 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public Vector3 direction;
-    public bool canMove;
+    public float speed, jumpForce, gravityMultiplier;
+    public Vector3 direction, jumpDirection;
 
+    public bool canJump;
     public Rigidbody rig;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody>();
+        canJump = false;
+    }
+
+    private void Update()
+    {
+        InputVerify();
+    }
+
+    void InputVerify()
+    {
+        if (Input.GetKey(KeybindingManager.Instance.keyLeft))
+        {
+            direction = Vector3.left;
+        }
+        else if (Input.GetKey(KeybindingManager.Instance.keyRight))
+        {
+            direction = Vector3.right;
+        }
+        
+        if (Input.GetKey(KeybindingManager.Instance.keyUp))
+        {
+            JumpPlayer();
+        }
     }
 
     private void FixedUpdate()
     {
-        if (canMove) {
-            MovePlayer();
+        MovePlayer(direction);
+
+        if (!canJump)
+        {
+            rig.AddForce(Vector3.up * Physics.gravity.y * gravityMultiplier, ForceMode.Acceleration);
         }
     }
 
-    void MovePlayer()
+    void MovePlayer(Vector3 dir)
     {
-        if (Input.GetKeyDown(KeybindingManager.Instance.keyLeft))
-        {
-            float rightDir = 1;
-            direction = new Vector3(rightDir, 0f, 0f);
+        rig.MovePosition(rig.position + dir * speed * Time.fixedDeltaTime);
+    }
 
-            rig.MovePosition(direction * speed * Time.deltaTime);
+    void JumpPlayer()
+    {
+        if (canJump)
+        {
+            rig.velocity = new Vector3(rig.velocity.x, 0, rig.velocity.z);
+
+            jumpDirection = Vector3.up * jumpForce;
+            rig.AddForce(jumpDirection, ForceMode.Impulse);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            canJump = false;
         }
     }
 }
